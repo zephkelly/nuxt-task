0e4aa86fe590fb9d133fee11db3130a42a6d690b
zephkelly <masterevank@gmail.com>
2025-09-30 22:49:42 +1000

    fix: add pathe as dependency

--- src/runtime/config.ts
+++ src/runtime/config.ts
@@ -1,56 +1,56 @@
-import type { RuntimeConfig } from 'nuxt/schema'
-import type { ModuleOptions } from './../module'
+import type { RuntimeConfig } from "nuxt/schema";
+import type { ModuleOptions } from "./../module";
 
 export const DEFAULT_MODULE_OPTIONS: ModuleOptions = {
-  serverTasks: true,
-  clientTasks: false,
-  experimental: {
-    tasks: false,
-  },
-  storage: {
-    type: 'memory',
-  },
-  timezone: {
-    type: 'UTC',
-    validate: true,
-    strict: false,
-  },
-} satisfies ModuleOptions
+    serverTasks: true,
+    clientTasks: false,
+    experimental: {
+        tasks: false,
+    },
+    storage: {
+        type: "memory",
+    },
+    timezone: {
+        type: "UTC",
+        validate: true,
+        strict: false,
+    },
+} satisfies ModuleOptions;
 
 export class ModuleConfiguration {
-  private options: ModuleOptions
+    private options: ModuleOptions;
 
-        constructor(moduleOptions: ModuleOptions) {
-    this.options = moduleOptions
+    constructor(moduleOptions: ModuleOptions) {
+        this.options = moduleOptions;
+    }
+
+    public setModuleOptions(options: ModuleOptions): void {
+        this.options = options;
+    }
+
+    public getModuleOptions(runtimeConfig?: RuntimeConfig): ModuleOptions {
+        if (runtimeConfig) {
+            this.options = runtimeConfig.cron;
         }
 
-  public setModuleOptions(options: ModuleOptions): void {
-    this.options = options
+        return this.options;
+    }
+
+    public static validateModuleOptions(options: ModuleOptions): boolean {
+        if (!options.timezone?.type) {
+            return false;
         }
 
-  public getModuleOptions(runtimeConfig?: RuntimeConfig): ModuleOptions {
-    if (runtimeConfig) {
-      this.options = runtimeConfig.cron
-                }
-
-    return this.options
+        if (options.experimental?.tasks && !options.serverTasks) {
+            console.warn(
+                "[ nuxt-task ] Experimental tasks require server tasks to be enabled"
+            );
+            return false;
         }
-
-  public static validateModuleOptions(options: ModuleOptions): boolean {
-    if (!options.timezone?.type) {
-      return false
-                }
-
-    if (options.experimental?.tasks && !options.serverTasks) {
-      console.warn(
-        "[ nuxt-task ] Experimental tasks require server tasks to be enabled"
-      );
-      return false
-                }
-    return true
-        }
+        return true;
+    }
 }
 
 export const moduleConfiguration = new ModuleConfiguration(
-  DEFAULT_MODULE_OPTIONS
-)
+    DEFAULT_MODULE_OPTIONS
+);
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,62 +1,60 @@
-import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin'
-import { createServerStorage } from './storage/server'
-import { Scheduler } from './scheduler'
-import { moduleConfiguration } from './config'
+import { defineNitroPlugin } from "nitropack/dist/runtime/plugin";
+import { createServerStorage } from "./storage/server";
+import { Scheduler } from "./scheduler";
+import { moduleConfiguration } from "./config";
 
-import { useRuntimeConfig } from '#imports'
+import { useRuntimeConfig } from "#imports";
 
 export default defineNitroPlugin(async (nitroApp) => {
-        const config = useRuntimeConfig()
-  const moduleOptions = config.cron || moduleConfiguration.getModuleOptions()
-  if (moduleOptions.experimental?.tasks) {
-                console.warn(
-                        "Experimental tasks are enabled, using Nitro's native task scheduler."
-                );
-                return;
-        }
+    const config = useRuntimeConfig();
+    const moduleOptions = config.cron || moduleConfiguration.getModuleOptions();
+    if (moduleOptions.experimental?.tasks) {
+        console.warn(
+            "Experimental tasks are enabled, using Nitro's native task scheduler."
+        );
+        return;
+    }
 
-        if (import.meta.test) {
-                console.log('Skipping scheduler in test environment')
-    return;
-        }
+    if (import.meta.test) {
+        console.log("Skipping scheduler in test environment");
+        return;
+    }
 
-        try {
-                const storage = await createServerStorage({ type: 'memory' })
-    const scheduler = new Scheduler(storage, moduleOptions, {
-                        tickInterval: 1000,
-                        maxConcurrent: 10,
-                        handleMissedTasks: true,
-                })
-
-    let taskDefinitions = []
     try {
-                        // @ts-ignore - we are using a virtual file
-                        const tasksVirtualFile = await import('#tasks')
-      taskDefinitions = tasksVirtualFile.taskDefinitions || []
+        const storage = await createServerStorage({ type: "memory" });
+        const scheduler = new Scheduler(storage, moduleOptions, {
+            tickInterval: 1000,
+            maxConcurrent: 10,
+            handleMissedTasks: true,
+        });
+
+        let taskDefinitions = [];
+        try {
+            // @ts-ignore - we are using a virtual file
+            const tasksVirtualFile = await import("#tasks");
+            taskDefinitions = tasksVirtualFile.taskDefinitions || [];
+        } catch {
+            console.warn("No tasks found, skipping scheduler.");
+            return;
+        }
+
+        for (const task of taskDefinitions) {
+            await scheduler.addTask({
+                name: task.meta.name,
+                options: { expression: task.schedule, ...task.options },
+                execute: task.run,
+            });
+        }
+
+        await scheduler.start();
+
+        nitroApp.hooks.hook("close", async () => {
+            await scheduler.stop();
+        });
+
+        // @ts-ignore - Dont know how to type the NitroApp
+        nitroApp.scheduler = scheduler;
+    } catch (error) {
+        console.error("Failed to initialize cron scheduler:", error);
     }
-                catch {
-                        console.warn('No tasks found, skipping scheduler.')
-      return;
-                }
-
-                for (const task of taskDefinitions) {
-                        await scheduler.addTask({
-                                name: task.meta.name,
-                                options: { expression: task.schedule, ...task.options },
-                                execute: task.run,
-                        })
-    }
-
-                await scheduler.start()
-
-    nitroApp.hooks.hook('close', async () => {
-                        await scheduler.stop()
-    })
-
-    // @ts-ignore - Dont know how to type the NitroApp
-    nitroApp.scheduler = scheduler
-  }
-        catch (error) {
-                console.error('Failed to initialize cron scheduler:', error)
-  }
-})
+});
